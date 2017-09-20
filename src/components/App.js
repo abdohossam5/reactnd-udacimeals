676a2c74f28236fe1f1ecb9919c03c68f266ddae
abdomokhtar <abdel@incredible-qr.com>
2017-09-20 09:24:07 +0800

    - feat: using provider to pass store

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import '../App.css';
-import {addRecipe} from "../actions/index"
-
+import {connect} from "react-redux";
+import {addRecipe, removeFromCalendar} from "../actions/index"
 class App extends Component {
 
     state = {
@@ -9,45 +9,38 @@ class App extends Component {
     };
 
     componentDidMount(){
-
-        this.setState(()=>({
-            calendar: this.props.store.getState()
-        }));
-
-        this.props.store.subscribe(() =>(
-            this.setState({
-                calendar: this.props.store.getState()
-            })
-        ))
+        console.log(this.props)
     }
 
     submitFood = () =>{
 
-        this.props.store.dispatch(addRecipe({
-            day: 'monday',
-            recipe:{label: this.input.value},
-            meal: 'breakfast'
-        }));
-
-        this.input.value = ''
     };
 
     render() {
         return (
-            <div>
-                <input
-                    type='text'
-                    ref={(input) => this.input = input}
-                    placeholder="Monday's Breakfast"
-                />
-                <button onClick={this.submitFood}>Submit</button>
-
-                <pre>
-          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
-        </pre>
-            </div>
+            <div>Hello react-redux</div>
         );
     }
 }
 
-export default App;
+function mapStateToProps(state, props) {
+    console.log(Object.keys(state));
+
+    return {
+        calendar: Object.keys(state).map(day => ({
+            day,
+            meals:{
+                ...state[day]
+            }
+        }))
+    }
+}
+
+function mapDispatchToProps() {
+    return {
+        addRecipe,
+        removeFromCalendar
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
