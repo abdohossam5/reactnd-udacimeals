8ee4f430f49c5172b47f9a907e36bad5c81f27a0
abdomokhtar <abdel@incredible-qr.com>
2017-09-28 00:01:53 +0800

    - feat: combine reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import {ADD_RECIPE, REMOVE_FROM_CALENDAR} from '../actions';
+import {combineReducers} from 'redux';
 
 const initialCalendarState = {
     sunday: {
@@ -76,4 +77,4 @@ function recipes(state={}, action) {
     }
 }
 
-export default calendar;
+export default combineReducers({calendar, recipes});
