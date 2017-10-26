667566f14da53426afab9e761f3c77b5295643a9
abdomokhtar <abdel@incredible-qr.com>
2017-10-26 15:09:26 +0800

    - feat: logger middleware

--- src/index.js
+++ src/index.js
@@ -3,14 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import reducer from './reducers';
 import {Provider} from "react-redux";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const logger = store => next => action => {
+    console.group('Logger');
+    console.log('dispatching => ', action);
+    let result = next(action);
+    console.log('state => ', store.getState());
+    console.groupEnd('Logger');
+    return result;
+};
+
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
+    composeEnhancers(applyMiddleware(logger))
 );
 
 ReactDOM.render(
